3d0c76ee3af9d7f27f5a0be1ac4017a6aa07f88c
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2020-11-04 02:53:15 +0100

    incoporated landing page

--- resources/views/layouts/fullLayoutMaster.blade.php
+++ resources/views/layouts/fullLayoutMaster.blade.php
@@ -28,7 +28,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title') - Frest - Bootstrap HTML admin template</title>
+    <title>@yield('title') PROPSEMPIRE</title>
     <link rel="apple-touch-icon" href="{{asset('images/ico/apple-icon-120.png')}}">
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/ico/favicon.ico')}}">
 
@@ -42,7 +42,7 @@ License: You must have a valid license purchased only from themeforest(the above
   @if($configData['theme'] === 'dark'){{'dark-layout'}} @elseif($configData['theme'] === 'semi-dark'){{'semi-dark-layout'}} @else {{'light-layout'}} @endif" data-open="click" data-menu="vertical-menu-modern" data-col="1-column">
     
     <!-- BEGIN: Header-->
-    @include('panels.navbar')
+   
     <!-- END: Header-->
     
     <!-- BEGIN: Content-->

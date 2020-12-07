581106b6caf9405e55951aa9a46cf6325d3590f9
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2020-12-07 10:02:33 +0100

    list form

--- resources/views/agents/add_prop2.blade.php
+++ resources/views/agents/add_prop2.blade.php
@@ -188,7 +188,7 @@
                 <div class="row">
                   
                 <div class="col-md-8"></div>
-                  <div class="col-md-4 p-1">
+                  <div class="col-md-4 p-2">
 
                     <button type="submit" class="btn btn-primary btn-block btn-lg shadow mr-1 mb-1">Next</button>
                    
